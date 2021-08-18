e1006d4ec5a6fbdd2b51d1dd6ab91d1a2a1977da
Nurtdinov1 <72566241+Nurtdinov1@users.noreply.github.com>
2021-08-18 15:07:11 +0300

    Add files via upload

---
+++ Dictionary.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <fstream>
+#include <regex>
+#include "converter.h"
+
+using namespace std;
+
+string ltrim(const string& str) {
+	return std::regex_replace(str, regex("^\\s+"), string(""));
+}
+
+string rtrim(const string& str) {
+	return regex_replace(str, regex("\\s+$"), string(""));
+}
+
+string trim(const string& str) {
+	return ltrim(rtrim(str));
+}
+
+
+int main()
+{
+	ofstream output_file;
+	output_file.open("ru_en.txt");
+	ofstream bad_words_file;
+	bad_words_file.open("bad_words.txt");
+
+	string input_file_name = "en_ru.txt";
+	ifstream input_file(input_file_name);
+	if (input_file.is_open())
+	{
+		string line;
+		string english_word;
+		vector<string> str_out;
+		while (getline(input_file, line))
+		{
+			if (!convert(line, english_word, str_out))
+			{
+				bad_words_file << line << endl;
+			}
+			else
+			{
+				for (size_t i = 0; i < str_out.size(); i++)
+				{
+					
+					line = trim(str_out[i]);
+					if (line.find(" ") == string::npos)
+					{
+						output_file << line << "\t" << english_word << endl;
+					}
+					else
+					{
+						bad_words_file << english_word << "\t" << line << endl;
+					}
+				}
+			}
+		}
+		bad_words_file.close();
+		output_file.close();
+		input_file.close();
+	}
+	else cout << "Unable to open input file";
+}
---
+++ converter.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+bool convert(string str_in, string& english_word, vector<string>& str_out)
+{
+	bool success = true;
+	str_out.clear();
+
+	size_t cursor = str_in.find("\t");
+	english_word = str_in.substr(0, cursor);
+	string tmp = str_in.substr(cursor + 1);
+
+	cursor = tmp.find_last_of("(");
+	while (cursor != string::npos)
+	{
+		size_t end_cursor = tmp.find(")", cursor + 1);
+		if (end_cursor == string::npos)
+		{
+			return false;
+		}
+		tmp.erase(cursor, end_cursor - cursor + 1);
+		cursor = tmp.find_last_of("(");
+	}
+	if (tmp.length() == 0)
+	{
+		return false;
+	}
+
+
+	string russian_word;
+	stringstream test(tmp);
+	while (getline(test, russian_word, ','))
+	{
+		str_out.push_back(russian_word);
+	}
+	if (str_out.size() == 0)
+	{
+		return false;
+	}
+
+	return success;
+}
